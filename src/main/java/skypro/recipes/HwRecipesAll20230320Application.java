6b703faeddde5c872c0d21d985a004331b2c254b
Qwaieare <gb-grand@mail.ru>
2023-03-21 20:09:42 +0300

    initial commit for hw_recipesAll_20230320

---
+++ src/main/java/skypro/recipes/HwRecipesAll20230320Application.java
@@ -0,0 +1,13 @@
+package skypro.recipes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HwRecipesAll20230320Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HwRecipesAll20230320Application.class, args);
+    }
+
+}
---
+++ src/test/java/skypro/recipes/HwRecipesAll20230320ApplicationTests.java
@@ -0,0 +1,13 @@
+package skypro.recipes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HwRecipesAll20230320ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
